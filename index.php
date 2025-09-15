40bf96d1eaaaa043e01426084ca122a8f532af3b
Romanovskyi2137 <113349151+Romanovskyi2137@users.noreply.github.com>
2025-09-15 11:37:26 +0300

    2nd test commit

--- index.php
+++ index.php
@@ -2,7 +2,6 @@
 <main>
     <?php get_template_part( "template-parts/components/main-page", "hero" ); ?>
     <?php get_template_part( "template-parts/components/main-page", "about" ); ?>
-    <p> just some changes to check git </p>
 </main>
 <?php get_footer(); ?>
 </html>
