2874dc1d10ebbd899e78353a965b7b32c967e734
Romanovskyi2137 <113349151+Romanovskyi2137@users.noreply.github.com>
2025-10-11 17:09:55 +0300

    instagram added + fonts, colors

--- template-parts/partials/partial-header-main.php
+++ template-parts/partials/partial-header-main.php
@@ -1,4 +1,24 @@
+<?php 
+    $settings_page_ID = get_page_by_path('site-settings')->ID;
+    $instagram_URL = get_post_meta($settings_page_ID, "instagram",true);
+    $instagram_icon_ID = get_post_meta($settings_page_ID, "instagram_icon", true);
+    $instagram_icon = wp_get_attachment_url($instagram_icon_ID);
+?>
 <header>
+    <a
+        <?php if ($instagram_URL) : ?>
+            href="<?php echo $instagram_URL ?>"
+        <?php endif ?>
+        target="blank"
+        class="social_icon"
+    >
+        <img 
+        <?php if ($instagram_icon) : ?>
+            src="<?php echo $instagram_icon ?>"
+        <?php endif ?>
+            alt="instagram icon"
+        />
+    </a>
     <div class="burger_btn menu_icon">
         <img src="<?php echo get_template_directory_uri(); ?>/assets/img/icons/burger-menu.svg" alt="mobile menu icon"/>
     </div>
