4822a5c2eefca8edbef9bd823aea303ce07b4eb2
Romanovskyi2137 <113349151+Romanovskyi2137@users.noreply.github.com>
2025-10-20 12:06:42 +0300

    inst, tel added to footer

--- template-parts/partials/partial-footer-main.php
+++ template-parts/partials/partial-footer-main.php
@@ -1,3 +1,8 @@
 <p>
+    <a href="" target="blank" rel="instagram" class="footer_instagram">Instagram</a>
+    <span> | </span>
+    <a href="tel:+380984826073" rel="tel" class="footer_tel">+380 98 482 60 73</a>
+</p>
+<p>
     © <?php echo date("Y"); ?> OlyaGera Photography | All rights reserved.
 </p>
