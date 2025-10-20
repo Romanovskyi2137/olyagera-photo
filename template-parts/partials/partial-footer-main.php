dd3c732bebf12aeff0f53638cc48f53cc3e257e4
Romanovskyi2137 <113349151+Romanovskyi2137@users.noreply.github.com>
2025-10-20 16:20:19 +0300

    hotfixes

--- template-parts/partials/partial-footer-main.php
+++ template-parts/partials/partial-footer-main.php
@@ -1,7 +1,14 @@
+<?php 
+    $settings_page_ID = get_page_by_path('site-settings')->ID;
+    $instagram_URL = get_post_meta($settings_page_ID, "instagram", true);
+    $phone_number = get_post_meta($settings_page_ID, "phone_number", true);
+    $phone_href = preg_replace('/[^\d+]/', '', $phone_number);
+?>
+
 <p>
-    <a href="" target="blank" rel="instagram" class="footer_instagram">Instagram</a>
+    <a href="<?php echo esc_url($instagram_URL); ?>" target="_blank" rel="noopener" class="footer_instagram">Instagram</a>
     <span> | </span>
-    <a href="tel:+380984826073" rel="tel" class="footer_tel">+380 98 482 60 73</a>
+    <a href="tel:<?php echo $phone_href; ?>" rel="tel" class="footer_tel"><?php echo esc_html($phone_number); ?></a>
 </p>
 <p>
     © <?php echo date("Y"); ?> OlyaGera Photography | All rights reserved.
